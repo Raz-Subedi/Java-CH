e8e583a87f986cbdcf7d9326866a7101808e5d39
Raz Subedi <subedirazkumar@gmail.com>
2025-01-25 17:56:39 +0545

    System.out.println()

---
+++ src/main/java/com/raz/Java_CH/sout/Test.java
@@ -0,0 +1,11 @@
+package com.raz.Java_CH.sout;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Rajkumar Subedi");
+
+    /*    System -> this is the final class from java.lang
+         out -> this is the static variable of the PrintStream
+        println() -> method inside the PrintStream Class.    */
+    }
+}
